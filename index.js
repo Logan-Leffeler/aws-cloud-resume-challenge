38ab2f389303b5bf747aa3708373d98e05a0bff9
Logan Leffeler <leffeler@gmail.com>
2023-05-02 20:00:21 -0500

    Add CICD for frontend

--- index.js
+++
@@ -1,8 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch("https://wx7orpt2kmeeuili5djyf4oyd40uviuv.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
-}
-
-updateCounter();
