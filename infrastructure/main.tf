d9bb6dddc2425963f326e0c315da713ac4b9543d
Logan Leffeler <leffeler@gmail.com>
2023-05-02 23:53:37 -0500

    add cloudfront to terraform file

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,59 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "logan-cloud-resume-challenge"
+  
+}
+
+
+resource "aws_dynamodb_table" "my_table" {
+  name              = "cloud-resume"
+  billing_mode      = "PAY_PER_REQUEST"
+  hash_key          = "id"
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+
+resource "aws_cloudfront_distribution" "my_distribution" {
+    enabled             = true
+    aliases             = ["resume.logan-leffeler.com"]
+    default_root_object = "index.html"
+    price_class         = "PriceClass_100"
+    is_ipv6_enabled     = true
+    viewer_certificate {
+      acm_certificate_arn      = "arn:aws:acm:us-east-1:611725109315:certificate/bd768ce7-7c43-4ba0-a1b6-6542d571fb60"
+      minimum_protocol_version = "TLSv1.2_2021"
+      ssl_support_method       = "sni-only"
+    }
+
+    restrictions {
+      geo_restriction {
+        locations        = []
+        restriction_type = "none"
+      }
+    }
+
+    origin {
+      domain_name              = "logan-cloud-resume-challenge.s3.us-east-1.amazonaws.com"
+      origin_id                = "logan-cloud-resume-challenge.s3.us-east-1.amazonaws.com"
+      origin_access_control_id = "EJIXJV90LN5S9"
+      connection_attempts      = 3
+      connection_timeout       = 10
+    }
+
+    default_cache_behavior {
+        target_origin_id       = "logan-cloud-resume-challenge.s3.us-east-1.amazonaws.com"
+        allowed_methods        = ["GET", "HEAD"]
+        cached_methods         = ["GET", "HEAD"]
+        viewer_protocol_policy = "https-only"
+        cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+        compress               = true
+    }
+  
+}
+
+
 resource "aws_lambda_function" "myfunc" {
     filename         = data.archive_file.zip.output_path
     source_code_hash = data.archive_file.zip.output_base64sha256
