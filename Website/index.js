5174276eef2f754b8ce30e2dfd3c81d068707e4f
Logan Leffeler <leffeler@gmail.com>
2023-05-02 21:18:17 -0500

    add some terraform configurations

--- Website/index.js
+++ Website/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://wx7orpt2kmeeuili5djyf4oyd40uviuv.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://mbbnruzaeir22m7e2e5erdop7u0hqhgk.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
 }
